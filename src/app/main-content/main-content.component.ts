d5c8867803a4a1a2a1fe7f54c944828ef50fb231
geckohack389 <xberic45@hotmail.com>
2018-11-22 18:19:22 -0800

    almost there but needs to acknowledge spacenot as a letter

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -11,7 +11,7 @@ export class MainContentComponent implements OnInit {
 
   // private scold: string;
   private input: string = ''; 
-  private alphabet: Array<string> = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  private alphabet: Array<string> = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   private output: string = '';
  
 
@@ -39,16 +39,18 @@ export class MainContentComponent implements OnInit {
         return letter == char.toLowerCase();
       });
       console.log("index", index)
-      if (index <= 12){
+      if (index => 0){
         output += this.alphabet[index+13]
         
-      }else {
+      }
+      else {
         index = 0;
         output += this.alphabet[index+13]
       }
+
     }
     console.log("output--->", output);
-    this.output = input;
+    this.output = output;
   }
 
 
