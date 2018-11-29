0d511338843ef348d2719d984447f84cf529ff60
geckohack389 <xberic45@hotmail.com>
2018-11-28 18:52:14 -0800

    Resolved problem with spacing

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -10,28 +10,27 @@ export class MainContentComponent implements OnInit {
 
 
   // private scold: string;
-  private input: string = ''; 
-  private alphabet: Array<string> = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  private input: string = '';
+  private alphabet: Array<string> = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   private output: string = '';
- 
+
 
 
 
   constructor() {
     // this.scold = "Getting closer";
-  
+
   }
-  
+
 
   ngOnInit() {
   }
 
   goConvert13(input: any) {
     console.log('This was a success', input);
+    input = input.trim().replace(/ /g, '');
     let output = "";
-    for(let i in input){ 
-      // console.log("i", i, "input", input);
-      // console.log("input[i]", input[i]);
+    for (let i in input) {
       let letter = input[i].toLowerCase();
       console.log("letter", letter);
       let index = this.alphabet.findIndex((char: string) => {
@@ -39,15 +38,13 @@ export class MainContentComponent implements OnInit {
         return letter == char.toLowerCase();
       });
       console.log("index", index)
-      if (index => 0){
-        output += this.alphabet[index+13]
+      if (index => 0) {
+        output += this.alphabet[index + 13].toLowerCase();
+      } else {
+        index = 0;
+        output += this.alphabet[index + 13].toLowerCase();
         
       }
-      else {
-        index = 0;
-        output += this.alphabet[index+13]
-      }
-
     }
     console.log("output--->", output);
     this.output = output;
@@ -55,5 +52,5 @@ export class MainContentComponent implements OnInit {
 
 
 
-  
+
 }
